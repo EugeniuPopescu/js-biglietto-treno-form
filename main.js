342665349b64c79020f4f16214b18a675d41ad21
Eugen <epopescuu@yahoo.it>
2023-10-27 17:14:33 +0200

    if condicion

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const fName = document.querySelector('#name');
-const km = document.querySelector('#km');
+const km = parseInt(document.querySelector('#km'));
 const age = document.querySelector('#age');
 const btnGenerate = document.querySelector('#generate');
 const btnCancel = document.querySelector('#cancel');
 const outName = document.querySelector('#outputName');
 
 btnGenerate.addEventListener('click', function() {
-    outName.innerHTML = fName.value;
+    // outName.innerHTML = fName.value;
+
+    if ( !Number.isFinite(km) ) {
+        outName.innerHTML = `Something went wrong`;
+    }
 });
