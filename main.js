5a8c29ef629f13983c408f863ea15634743618b1
Eugen <epopescuu@yahoo.it>
2023-10-30 01:20:29 +0100

    add .value and finish exercise

--- main.js
+++ main.js
@@ -1,57 +1,66 @@
-const inputName = document.querySelector('#name');
-const inputKm = parseInt(document.querySelector('#inputKm'));
-const age = document.querySelector('#age');
+let SELECTORS = {
+    inputName: document.querySelector('#name'),
+    inputKm: document.querySelector('#km'),
+    age: document.querySelector('#age'),
+    
+    btnGenerate: document.querySelector('#generate'),
+    btnCancel: document.querySelector('#cancel'),
+    
+    outName: document.querySelector('#outputName'),
+    outSale: document.querySelector('#outputSale'),
+    outCarroza: document.querySelector('#carrozza'),
+    outCodice: document.querySelector('#codice'),
+    outCostoBiglietto: document.querySelector('#costoBiglietto')
+};
 
-const btnGenerate = document.querySelector('#generate');
-const btnCancel = document.querySelector('#cancel');
-
-const outName = document.querySelector('#outputName');
-const outSale = document.querySelector('#outputSale');
-const outCarroza = document.querySelector('#carrozza');
-const outCodice = document.querySelector('#codice');
-const outCostoBiglietto = document.querySelector('#costoBiglietto');
-
-let price = inputKm * 0.21
 let distance;
 let strangeAge;
 let finalPrice;
 let sale; 
 let saleMessage;
 
-btnGenerate.addEventListener('click', function() {
+SELECTORS.btnGenerate.addEventListener('click', function() {
     // outName.innerHTML = inputName.value;
+    // var inputKmValue = parseInt(inputKm.value);
+    let price = parseInt(SELECTORS.inputKm.value) * 0.21;
 
     // controllo se non inserisco dati
-    if ( inputName.value=="" && !Number.isFinite(inputKm) ) {
-        console.log('ciao');
-        // outName.innerHTML = `Something went wrong`;
+    if (SELECTORS.inputName.value === "" && !Number.isFinite(SELECTORS.inputKm)) {
         alert('Something went wrong');
 
-        
-        console.log(finalPrice);
-
         // partono i controlli
-    } else {        
-        // prezzo finale
-        if ( age.value=='Under 18' ) {
-            sale = (price / 100) * 20;
-            finalPrice = price - sale;
-            saleMessage = `Sconto under 18 (${price}€ - ${sale.toFixed(2)}€ (sconto))`;
-            
-        } else if( age.value=='Over 65' ) {
-            sale = (price / 100) * 40;
-            finalPrice = price - sale;
-            saleMessage = `Sconto over 65 (${price}€ - ${sale.toFixed(2)}€ (sconto))`;
-            
-        } else {
-            finalPrice = price;
-            saleMessage = `Biglietto Standard`;
-        }
+    } else {   
+        // var currentAge = age && age.value? age.value: 'value null'; 
+        
+        // controllo distanza
+        if(SELECTORS.inputKm.value > 1000) {
+            SELECTORS.outCostoBiglietto.innerHTML = `ERROR non superiamo i 1000Km`;
 
-        outName.innerHTML = inputName.value.toUpperCase();
-        outSale.innerHTML = `${saleMessage}`;
-        outCarroza.innerHTML = Math.floor(Math.random() * 9) + 1;
-        outCodice.innerHTML = Math.floor(Math.random() * 99999) + 1;
-        outCostoBiglietto.innerHTML = `${finalPrice.toFixed(2)}`;
+        } else {
+            if(SELECTORS.age.value === 'Under-18') {
+            sale = (price / 100) * 20;
+                finalPrice = price - sale;
+                saleMessage = `Sconto under 18 (${price}€ - ${sale.toFixed(2)}€)`;
+                
+            } else if(SELECTORS.age.value === 'Over-65' ) {
+                sale = (price / 100) * 40;
+                finalPrice = price - sale;
+                saleMessage = `Sconto over 65 (${price}€ - ${sale.toFixed(2)}€)`;
+                
+            } else {
+                finalPrice = price;
+                saleMessage = `Biglietto Standard`;
+            }
+            SELECTORS.outName.innerHTML = SELECTORS.inputName.value.toUpperCase();
+            SELECTORS.outSale.innerHTML = `${saleMessage}`;
+            SELECTORS.outCarroza.innerHTML = Math.floor(Math.random() * 9) + 1;
+            SELECTORS.outCodice.innerHTML = Math.floor(Math.random() * 99999) + 1;
+            SELECTORS.outCostoBiglietto.innerHTML = `${finalPrice.toFixed(2)}€`;
+        }
     }
 });
+
+// click btnCancel to recharge page
+SELECTORS.btnCancel.addEventListener('click', function() {
+    window.location.reload()
+})
